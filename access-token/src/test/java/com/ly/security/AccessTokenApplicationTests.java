9aca9956003eb4a84d5b517045a2cd48fbdf3bc1
kerian <james342506@gmail.com>
2025-07-05 21:37:29 +0630

    project starter

---
+++ access-token/src/test/java/com/ly/security/AccessTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ly.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
